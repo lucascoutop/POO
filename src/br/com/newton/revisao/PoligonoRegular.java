59b75df2537cb89005781c1cf320861c11793586
lucascoutop <105723795+lucascoutop@users.noreply.github.com>
2023-04-11 10:50:59 -0300

    Add files via upload

---
+++ src/br/com/newton/revisao/PoligonoRegular.java
@@ -0,0 +1,19 @@
+package br.com.newton.revisao;
+
+public abstract class PoligonoRegular {
+
+	protected int numLados;
+    protected double tamLado;
+
+    public PoligonoRegular(int numLados, double tamLado) {
+        this.numLados = numLados;
+        this.tamLado = tamLado;
+    }
+
+    public abstract double calcularArea();
+
+    public double calcularPerimetro() {
+        return numLados * tamLado;
+    }
+	
+}
